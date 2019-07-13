4cd23f2c30b772574cb3ca166c53623c1322b752
bustohector <52847300+bustohector@users.noreply.github.com>
2019-07-13 01:28:17 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Movimiento Japay Oñondivepa</title>
+</head>
+
+<body>
+
+<embed src="japayonondive/InformeCajaBancaria.pdf" type="application/pdf" width="100%" height="600px" />
+
+
+</body>
+</html>
